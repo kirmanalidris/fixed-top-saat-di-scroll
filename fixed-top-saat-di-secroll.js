cfe903f18474002c12a602a59a2771a660b075b2
kirmanalidris <56144888+kirmanalidris@users.noreply.github.com>
2019-10-22 10:03:42 +0700

    Create fixed-top-saat-di-secroll.js
    Javascript untuk membuat nav atau yang lainnya,
    Gunakan id "kirman-scrool-fixed-top" agar javascript berpungsi, contoh:
    
    <header id="kirman-scrool-fixed-top" class="?">
    ==================
    </header>

---
+++ fixed-top-saat-di-secroll.js
@@ -0,0 +1,18 @@
+  <script>
+	var nav = document.getElementById('kirman-scrool-fixed-top');
+
+       window.onscroll = function () {
+
+         if(window.pageYOffset > 100){
+
+          nav.style.position = "fixed";
+          nav.style.top = 0;
+
+          }else{
+            // nav.style.position = "absolute";
+            nav.style.position = 'relative'; //fixed
+            nav.style.top = 100;
+          }
+       }
+  </script>
+</html>
